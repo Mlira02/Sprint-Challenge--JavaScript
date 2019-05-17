b91ccf890c41b24a941cbfdd0e8cdaf55642a799
Allen Babiarz <ababiarz@Allen-Babiarzs-iMac.local>
2019-05-17 12:22:08 -0500

    Almost complete minus .foreach method

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -122,9 +122,11 @@ zooAnimals = [{"animal_name":"Jackal, asiatic","population":5,"scientific_name":
 The zoo wants to display both the scientific name and the animal name in front of the habitats.  Return an array with only the animal and scientific names in it.  The individual values in the array should look like this "Name: Jackal, asiatic, Scientific: Canis aureus."
 
 */
-const animalNames = []
+const animalNames = zooAnimals.forEach((name) => {
+  console.log (name.animal_name + name.scientific_name)
+});
 
-console.log(animalNames);
+console.log (animalNames);
 /* Request 2: .map()    
 
 The zoos need a list of all their animal's names (names only, not scientific) converted to lower case.  Create a new array named lowerCase and map over each name to convert them all to lower case.  Log the resut.
